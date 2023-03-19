2cbd4a391ea6af007c989525c8e9bbf121ba33de
gustavo <gustavoromeroj521@gmail.com>
2023-03-19 09:06:53 -0500

    Puestos-Formulario”
    git commit -m Puestos-Formulario”

--- secciones/puestos/index.php
+++ secciones/puestos/index.php
@@ -1,4 +1,33 @@
-<?php include("../../templates/header.php");?>
+<?php include("../../templates/header.php"); ?>
+<br />
 
-listar puestos
-<?php include("../../templates/footer.php");?>
+<div class="card">
+    <div class="card-header">
+        <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Agregar Registro</a>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive-sm">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Nombre del Puesto</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="">
+                        <td scope="row">1</td>
+                        <td>Programador Junior</td>
+                        <td>
+                            <input name="btneditar" id="btneditar" class="btn btn-info" type="button" value="Editar">
+                            <input name="btneliminar" id="btneliminar" class="btn btn-danger" type="button" value="Eliminar">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php include("../../templates/footer.php"); ?>

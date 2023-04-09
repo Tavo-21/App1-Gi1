bede78ecb98fc6f0183eba62b95ee417b47c9828
gustavo <gustavoromeroj521@gmail.com>
2023-04-08 20:51:12 -0500

    Eliminar Empleadosos con los archivos adjuntos, Recuperacion de datos para Actulizar Empleado

--- secciones/empleados/editar.php
+++ secciones/empleados/editar.php
@@ -1,4 +1,101 @@
-<?php include("../../templates/header.php");?>
+<?php
+include("../../bd.php");
+if (isset($_GET['txtID'])) {
+    $txtID = (isset($_GET['txtID']) ? $_GET['txtID'] : '');
+    $sentencia = $conexion->prepare("SELECT * FROM tbl_empleados WHERE id=:id");
+    $sentencia->bindParam('id', $txtID);
+    $sentencia->execute();
+    $registro = $sentencia->fetch(PDO::FETCH_LAZY); //fetch lazy para que solo se cargue un registro
 
-Editar Empleados
-<?php include("../../templates/footer.php");?>
+    $primernombre = $registro["primernombre"];
+    $segundonombre = $registro["segundonombre"];
+    $primerapellido = $registro["primerapellido"];
+    $segundoapellido = $registro["segundoapellido"];
+    $foto = $registro["foto"];
+    $cv = $registro["cv"];
+    $idpuesto = $registro["idpuesto"];
+    $fechadeingreso = $registro["fechadeingreso"];
+
+    $sentencia = $conexion->prepare("SELECT *FROM tbl_puestos");
+    $sentencia->execute();
+    $lista_tbl_puestos = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+}
+?>
+<?php include("../../templates/header.php"); ?>
+<br />
+<div class="card">
+    <div class="card-header">
+        Editar Datos del Empleados
+    </div>
+    <div class="card-body">
+        <!--enctype es la instruccion junto con multipart/form data para adjuntar archivos--->
+        <form action="" method="post" enctype="multipart/form-data">
+
+            <div class="mb-3">
+                <label for="txtID" class="form-label">Id</label>
+                <input type="text" value="<?php echo $txtID ?>" class="form-control" readonly name="txtID" id="txtID" aria-describedby="helpId" placeholder="Id">
+            </div>
+
+            <div class="mb-3">
+                <label for="primernombre" class="form-label">Primer Nombre</label>
+                <input type="text" value="<?php echo $primernombre ?>" class="form-control" name="primernombre" id="primernombre" aria-describedby="helpId" placeholder="Primer Nombre">
+            </div>
+
+            <div class="mb-3">
+                <label for="segundonombre" class="form-label">Segundo Nombre</label>
+                <input type="text" value="<?php echo $segundonombre ?>" class="form-control" name="segundonombre" id="segundonombre" aria-describedby="helpId" placeholder="Segundo Nombre">
+            </div>
+
+            <div class="mb-3">
+                <label for="primerapellido" class="form-label">Primer Apellido</label>
+                <input type="text" value="<?php echo $primerapellido ?>" class="form-control" name="primerapellido" id="primerapellido" aria-describedby="helpId" placeholder="Primer Apellido">
+            </div>
+
+            <div class="mb-3">
+                <label for="segundoapellido" class="form-label">Segundo Apellido</label>
+                <input type="text" value="<?php echo $segundoapellido ?>" class="form-control" name="segundoapellido" id="segundoapellido" aria-describedby="helpId" placeholder="Segundo Apellido">
+            </div>
+
+            <div class="mb-3">
+                <label for="foto" class="form-label">Foto</label>
+                <img width="100" src="<?php echo $foto; ?>" class="rounded" alt="">
+                <br><br> 
+                <input type="file" class="form-control" name="foto" id="foto" aria-describedby="helpId" placeholder="Foto">
+            </div>
+
+            <div class="mb-3">
+                <label for="cv" class="form-label">CV(PDF)</label>
+                <br/>
+                <a href="<?php echo $cv;?>"><?php echo $cv;?></a>
+                <input type="file" class="form-control" name="cv" id="cv" aria-describedby="helpId" placeholder="CV">
+            </div>
+
+            <div class="mb-3">
+                <label for="idpuesto" class="form-label">Puesto:</label>
+                <select class="form-select form-select-sm" name="idpuesto" id="idpuesto">
+                    <?php foreach ($lista_tbl_puestos as $registros) { ?>
+                        <!--Evaluar que se seleccione
+                        el select, seleciona el valor que coincida con el perfil que se esta consultando
+                        como tal es si existe el id que estamos consultando en la base de datos y evlua ? si conincide para{
+                        imprimirlo en el selecte de lo contrario no imprimi nada-->
+                        <option <?php echo ($idpuesto == $registros['id']) ? "selected" : ""; ?> value="<?php echo $registros['id'] ?>">
+                            <?php echo $registros['nombredelpuesto']; ?></option>
+                    <?php
+                    } ?>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label for="fechadeingreso" class="form-label">Fecha de Ingreso</label>
+                <input type="date" value="<?php echo $fechadeingreso ?>" class="form-control" name="fechadeingreso" id="fechadeingreso" aria-describedby="emailHelpId" placeholder="Fecha de Ingreso">
+            </div>
+
+            <button type="submit" class="btn btn-success">Actualizar registro</button>
+            <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+        </form>
+    </div>
+    <div class="card-footer text-muted">
+        Footer
+    </div>
+</div>
+<?php include("../../templates/footer.php"); ?>
--- secciones/empleados/index.php
+++ secciones/empleados/index.php
@@ -1,12 +1,44 @@
 <?php
 include("../../bd.php");
-$sentencia = $conexion->prepare("SELECT *,
 
+#Obtener y borrar
+if(isset($_GET['txtID'])){
+    
+    $txtID=(isset($_GET['txtID'])?$_GET['txtID'] : '');
+    //buscar archivo relacionado con el empleado 
+    $sentencia=$conexion->prepare("SELECT foto, cv FROM tbl_empleados WHERE id=:id");
+    $sentencia->bindParam(":id",$txtID);
+    $sentencia->execute();
+    $registro_recuperado=$sentencia->fetch(PDO::FETCH_LAZY);
+    echo print_r($registro_recuperado);
+
+    //preugntar si el archivo existe
+    if(isset($registro_recuperado["foto"]) && $registro_recuperado["foto"]!=""){
+        if(file_exists("./".$registro_recuperado["foto"])){
+            unlink("./".$registro_recuperado["foto"]);
+        }
+    }
+
+    if(isset($registro_recuperado["cv"]) && $registro_recuperado["cv"]!=""){
+        if(file_exists("./".$registro_recuperado["cv"])){
+            unlink("./".$registro_recuperado["cv"]);
+        }
+    }
+
+    #borrado
+    $sentencia=$conexion->prepare("DELETE FROM tbl_empleados WHERE id=:id");
+    $sentencia->bindParam(":id",$txtID);
+    $sentencia->execute();
+    header("location: index.php");
+
+}
+
+
+$sentencia = $conexion->prepare("SELECT *,
 #hacer una subconsulta para sacar el valor de puesto
 (SELECT nombredelpuesto 
 FROM tbl_puestos 
 WHERE tbl_puestos.id=tbl_empleados.idpuesto limit 1) as puesto 
-
 FROM tbl_empleados");
 $sentencia->execute();
 $lista_tbl_empleados = $sentencia->fetchAll(PDO::FETCH_ASSOC);
@@ -45,16 +77,16 @@ $lista_tbl_empleados = $sentencia->fetchAll(PDO::FETCH_ASSOC);
                                 <?php echo $registro['primerapellido']; ?>
                                 <?php echo $registro['segundoapellido'] ?></td>
                             <td>
-                            <img width="90" 
-                            src="<?php echo $registro['foto'];?>" class="img-fluid rounded" alt="">
+                                <img width="90" src="<?php echo $registro['foto']; ?>" class="img-fluid rounded" alt="">
                             </td>
                             <td><?php echo $registro['cv'] ?></td>
-                            <!--Se hizo una subconsulta para sacar el nombre del puesto--> 
+                            <!--Se hizo una subconsulta para sacar el nombre del puesto-->
                             <td><?php echo $registro['puesto'] ?></td>
                             <td><?php echo $registro['fechadeingreso'] ?></td>
                             <td><a name="" id="" class="btn btn-primary" href="#" role="button">Carta</a>
-                                <a name="" id="" class="btn btn-info" href="#" role="button">Editar</a>
-                                <a name="" id="" class="btn btn-danger" href="#" role="button">Eliminar</a>
+                                <a class="btn btn-info" href="editar.php?txtID=<?php echo $registro['id']; ?>" role="button">Editar</a>
+                                <a class="btn btn-danger" href="index.php?txtID=<?php echo $registro['id']; ?>" role="button">Eliminar</a>
+                                <!--Esta enviando un dato id a traves de la url y quien lo hace es la url eliminar-->
                             </td>
                         </tr>
                     <?php } ?>
